a16fbe28283255ea606d49ef5cfb650cf65429e0
hanyutao_gs <876924410@qq.com>
2020-05-13 17:53:42 +0800

    完成首页和一半订餐页面

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -7,7 +7,9 @@ Page({
   },
   onLoad: function () {
     this.setData({
-   
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
     })
   }
 })
